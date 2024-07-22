bf9d71967f50d93d8c98edda72324cc8f33f72a3
HMonique <161358207+HMonique@users.noreply.github.com>
2024-07-22 09:50:37 -0400

    Commit

--- src/EnhancedForLoop.java
+++ src/EnhancedForLoop.java
@@ -1,4 +1,4 @@
-public class EnhanceForLoop {
+public class EnhancedForLoop {
     public static void main(String[] args) {
         String[] names = { "New York", "Dallas", "Las Vegas", "Florida"};
         for (String name : names) {
